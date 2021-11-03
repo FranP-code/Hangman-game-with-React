5bc2176a907919d611e59f48b3fabc04bbd5e7c9
Francisco Pessano <franpessano1@gmail.com>
2021-11-03 19:15:55 -0300

    Restored footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 const Footer = (props) => {
     return (
         <footer className={props.darkMode ? 'made-with-love dark-mode-component' : 'made-with-love'}>
-            {/* <p>
-                Made with 💓 by <a href="http://www.franp.xyz">Francisco Pessano</a>.
-            </p> */}
+            <p>
+                Made with 💓 by <Link className="link" to='http://www.franp.xyz'>Francisco Pessano</Link>{/*<a href="http://www.franp.xyz">Francisco Pessano</a>*/}.
+            </p>
 
             <Link className="link" to='/credits-page'>Credits</Link>
         </footer>
